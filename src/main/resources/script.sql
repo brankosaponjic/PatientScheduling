dc5d5d8e3647038a0f09180852015678c0255e4d
Branko <brankosaponjic76@gmail.com>
2021-02-18 16:55:18 +0100

    Added sql script and datasource parameters. Closes #1.

---
+++ src/main/resources/script.sql
@@ -0,0 +1,42 @@
+create database mydb;
+
+use mydb;
+
+create table patient(
+id int primary key auto_increment,
+first_name varchar(20),
+last_name varchar(30),
+provider_name varchar(30),
+phone varchar(30),
+copay decimal(10,5)
+);
+
+create table doctor(
+id int primary key auto_increment,
+first_name varchar(20),
+last_name varchar(30),
+speciality varchar(30)
+);
+
+create table patients_doctors(
+patient_id int,
+doctor_id int,
+foreign key (patient_id)
+references patient(id),
+foreign key (doctor_id)
+references doctor(id)
+);
+
+create table appointment(
+id int primary key auto_increment,
+patient_id int,
+doctor_id int,
+appointment_date datetime,
+started tinyint(1),
+ended tinyint(1),
+reason varchar(200),
+foreign key (patient_id)
+references patient(id),
+foreign key (doctor_id)
+references doctor(id)
+);
