2e651feced73f6ee0a3eae0dbc339c6eef225c81
Branko <brankosaponjic76@gmail.com>
2021-02-22 11:32:40 +0100

    Test Appointment Created. Closes #10

--- src/test/java/brankosaponjic/patientscheduling/PatientschedulingApplicationTests.java
+++ src/test/java/brankosaponjic/patientscheduling/PatientschedulingApplicationTests.java
@@ -1,17 +1,15 @@
 package brankosaponjic.patientscheduling;
 
-import brankosaponjic.patientscheduling.entities.Doctor;
-import brankosaponjic.patientscheduling.entities.Insurance;
-import brankosaponjic.patientscheduling.entities.Patient;
+import brankosaponjic.patientscheduling.entities.Appointment;
+import brankosaponjic.patientscheduling.repositories.AppointmentRepository;
 import brankosaponjic.patientscheduling.repositories.DoctorRepository;
 import brankosaponjic.patientscheduling.repositories.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.sql.Timestamp;
+import java.util.Date;
 
 @SpringBootTest
 class PatientschedulingApplicationTests {
@@ -22,45 +20,21 @@ class PatientschedulingApplicationTests {
     @Autowired
     PatientRepository patientRepository;
 
+    @Autowired
+    AppointmentRepository appointmentRepository;
+
     @Test
-    void testCreatePatient() {
+    void testCreateAppointment() {
+        Appointment appointment1 = new Appointment();
+        Timestamp appointmentTime = new Timestamp(new Date().getTime());
+        appointment1.setAppointmentTime(appointmentTime);
+        appointment1.setReason("I have lung inflammation");
+        appointment1.setStarted(true);
+        appointment1.setPatient(patientRepository.findById(4L).get());
+        appointment1.setDoctor(doctorRepository.findById(1L).get());
 
-        // patient 1
-        Patient patient1 = new Patient();
-        patient1.setFirstName("Petar");
-        patient1.setLastName("Petrović");
-        patient1.setPhone("+381662277448");
+        appointmentRepository.save(appointment1);
 
-        Insurance insurance1 = new Insurance();
-        insurance1.setProviderName("Dunav osiguranje");
-        insurance1.setCopay(20d);
-        patient1.setInsurance(insurance1);
-
-        Optional<Doctor> doctor1 = doctorRepository.findById(1L);
-
-        List<Doctor> doctors1 = Arrays.asList(doctor1.get());
-
-        patient1.setDoctors(doctors1);
-
-        // patient 2
-        Patient patient2 = new Patient();
-        patient2.setFirstName("Jovan");
-        patient2.setLastName("Jovanović");
-        patient2.setPhone("+381691137548");
-
-        Insurance insurance2 = new Insurance();
-        insurance2.setProviderName("Generali osiguranje");
-        insurance2.setCopay(10d);
-        patient2.setInsurance(insurance2);
-
-        Optional<Doctor> doctor2 = doctorRepository.findById(2L);
-
-        List<Doctor> doctors2 = Arrays.asList(doctor2.get());
-
-        patient2.setDoctors(doctors2);
-
-        patientRepository.save(patient1);
-        patientRepository.save(patient2);
     }
 
 }
